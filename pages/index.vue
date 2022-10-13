3580cba8cf205e888105702e388963173c651d4f
高橋 勇気 <y.takahashi@AEM206.local>
2022-10-13 11:27:36 +0900

    デフォルトのnuxt-pj

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+div
+  tutorial
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  computed: {},
+  mounted() {
+    console.log('test');
+    this.test();
+  },
+  methods: {
+    test() {
+      console.log('methods test');
+    }
+  }
+}
+</script>
