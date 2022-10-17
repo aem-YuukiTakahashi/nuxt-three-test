43b5b9fb07fa2896000b1628e43a6c8bb39af039
高橋 勇気 <y.takahashi@AEM206.local>
2022-10-17 17:14:37 +0900

    svgアニメーション

---
+++ pages/demo/blob.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+div
+  .blob
+    img(src='/blob.svg')
+</template>
+
+<style scoped>
+.blob {
+  width: calc(100% - 20px);
+  max-width: 500px;
+  margin: 0 auto;
+  animation:30s linear infinite rotation1;
+}
+
+@keyframes rotation1{
+  0%{ transform:rotate(0);}
+  100%{ transform:rotate(360deg); }
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  computed: {},
+  mounted() {
+  },
+  methods: {
+  }
+}
+</script>
